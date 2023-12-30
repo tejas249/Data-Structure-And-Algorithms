3398f00d6dcda5609a5428a97a591e994285b5d3
Tejas Kamble <tejaskambletk132@gmail.com>
2023-12-30 11:18:24 +0530

    Create max_depth_BT_LC104.cpp
    Height of binary tree LC104

---
+++ BinaryTree/max_depth_BT_LC104.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root){
+            
+            
+            int left = 1+maxDepth(root->left);
+            int right = 1+maxDepth(root->right);
+            return max(left, right);
+        }
+        return 0;
+
+        
+
+        
+    }
+};
