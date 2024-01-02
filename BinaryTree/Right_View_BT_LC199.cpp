a83b2091190afb318fd3e8f139d82a3f7f2fd7a2
Tejas Kamble <tejaskambletk132@gmail.com>
2024-01-02 12:05:13 +0530

    Create Right_View_BT_LC199.cpp
    LC 199

---
+++ BinaryTree/Right_View_BT_LC199.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+     
+      
+    void recursion(TreeNode* root , int level, vector<int> &ans){
+      
+        // base case
+        if(root == NULL){
+            return;
+        }
+      
+        //if ans == level hoga to add karo 
+        if(level == ans.size()){
+            ans.push_back(root->val);
+        }
+      
+        // recursive call for traversal
+        recursion(root->right,level+1,ans);
+        recursion(root->left,level+1,ans);
+    }
+
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        recursion(root,0, ans);
+        return ans;
+    }
+};
