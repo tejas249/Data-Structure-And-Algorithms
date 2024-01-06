6f29e3fcb41b3bb720f28705366b848cb5a56480
Tejas Kamble <tejaskambletk132@gmail.com>
2024-01-06 11:39:40 +0530

    Update Valid_BST_LC98.cpp
    LC 98 Validate BST

--- Binary_Search_Tree/Valid_BST_LC98.cpp
+++ Binary_Search_Tree/Valid_BST_LC98.cpp
@@ -23,3 +23,50 @@ public:
     return isValidBST(root, LLONG_MIN, LLONG_MAX);
     }
 };
+
+
+SOLUTION NO 2 
+    
+    
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool solve(TreeNode* root, long long int lowerbound , long long int upperbound){
+        //base case
+        if(root == NULL){
+            return true;
+        }
+
+        //conditions
+        bool cond1 = (root->val > lowerbound);
+        bool cond2 = (root->val < upperbound);
+
+        // left and right ans nikalo
+        bool leftans = solve(root->left,lowerbound ,root->val);
+        bool rightans = solve(root->right, root->val,upperbound );
+
+        if(cond1 && cond2 && leftans && rightans)
+            return true;
+        else
+            return false;
+        
+
+    }
+    bool isValidBST(TreeNode* root) {
+        long long int lowerbound = -2147483657;
+        long long int upperbound = 2147483657;
+        bool ans = solve(root, lowerbound, upperbound);
+        return ans;
+    }
+};
+    
