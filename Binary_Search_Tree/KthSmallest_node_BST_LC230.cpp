f01e2b228a70ad0c5c0c19ceb2f28dd0a9f1dccd
Tejas Kamble <tejaskambletk132@gmail.com>
2024-01-07 10:54:21 +0530

    Create KthSmallest_node_BST_LC230.cpp
    LC230

---
+++ Binary_Search_Tree/KthSmallest_node_BST_LC230.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int &k) {
+
+        //make sure to pass k by referenec 
+        //base case 
+        if(root == NULL){
+            return -1;
+        }
+
+        //left side 
+        int leftans = kthSmallest(root->left, k);
+        if(leftans != -1){
+            return leftans;
+        }
+        k--;
+
+        //node 
+        if(k == 0){
+            return root->val;
+        }
+        //right call 
+        int rightans = kthSmallest(root->right, k);
+        return rightans;
+
+
+    }
+};
