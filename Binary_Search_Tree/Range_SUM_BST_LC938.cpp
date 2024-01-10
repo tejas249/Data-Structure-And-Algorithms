da45585c0651da8ce9a52271579ba25cc08e8442
Tejas Kamble <tejaskambletk132@gmail.com>
2024-01-10 19:27:40 +0530

    Update Range_SUM_BST_LC938.cpp
    LC 938 SOL 2

--- Binary_Search_Tree/Range_SUM_BST_LC938.cpp
+++ Binary_Search_Tree/Range_SUM_BST_LC938.cpp
@@ -46,3 +46,44 @@ public:
 
     }
 };
+
+
+
+
+
+
+
+// APPROACH NO 2 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    //Method 2 solution 
+    void solve(TreeNode* root, int low, int high, int &sum){
+        if(!root) return;
+        //LEFT CALL
+        solve(root->left, low, high, sum);
+
+        //CHECK IF ITS IN THE RANGE THEN ADD IT 
+        if(root->val >= low && root->val <= high){
+            sum = sum + root->val;
+        }
+        //RIGHT CALL 
+        solve(root->right, low, high, sum);
+
+    }
+    int rangeSumBST(TreeNode* root, int low, int high) {
+    int sum = 0;
+    solve(root,low,high,sum);
+    return sum;
+    }
+};
